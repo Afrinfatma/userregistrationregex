584c11959f5f56aaa8fd1a30b718c99a7365b57d
Afrinfatma <afrinfatma4@gmail.com>
2022-08-03 15:59:08 +0530

    Added:uc3 ie email regex

--- UserRegistration.java
+++ UserRegistration.java
@@ -6,6 +6,7 @@ import java.util.regex.Pattern;
 public class UserRegistration {
 	static String firstNameRegex="^[A-Za-z]{3,}";
 	static String lastNameRegex="^[A-Za-z]{3,}";
+	static String emailRegex="^[A-za-z0-9]+([.+-][A-za-z0-9]+)?[@][a-z0-9]+[.][a-z]{2,3}([.][a-z]{2})?$";
       static Pattern pattern;
       static Matcher matcher;
       
@@ -14,14 +15,23 @@ public class UserRegistration {
     	  matcher=pattern.matcher(firstName);
     	  return matcher.matches();
       }
+
       public static boolean lastName(String lastName) {
-   	   pattern =Pattern.compile(lastNameRegex);
-   	  matcher=pattern.matcher(lastName);
-   	  return matcher.matches();
-     }
+      	   pattern =Pattern.compile(lastNameRegex);
+      	  matcher=pattern.matcher(lastName);
+      	  return matcher.matches();
+      	  
+        }
+      public static boolean email(String email) {
+      	   pattern =Pattern.compile(emailRegex);
+      	  matcher=pattern.matcher(email);
+      	  return matcher.matches();
+      	  
+        }
 	public static void main(String[] args) {
-		System.out.println("First Name=" +firstName("afrin"));
-		System.out.println("First Name=" +lastName("Fatma"));
+		System.out.println("First Name=" +firstName("Afrin"));
+		System.out.println("Last Name=" +lastName("Fatma"));
+		System.out.println("Email=" +email("abc.xyz@bridgelabz.co.in"));
 	}
 
 }
